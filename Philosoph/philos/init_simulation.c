81ce9dc986f92dfb205237a5c3bb0f58921fe333
Albert01H <albertoamuyela@gmail.com>
2024-11-18 10:05:39 +0100

    All problems solved

---
+++ Philosoph/philos/init_simulation.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_simulation.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahamuyel <ahamuyel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/10 16:50:40 by ahamuyel          #+#    #+#             */
+/*   Updated: 2024/11/15 14:54:29 by ahamuyel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	take_input(t_philosoph *philos, char **av)
+{
+	philos->number_of_philos = ft_atoi(av[1]);
+	philos->time_to_die = ft_atoi(av[2]);
+	philos->time_to_eat = ft_atoi(av[3]);
+	philos->time_to_sleep = ft_atoi(av[4]);
+	if (av[5])
+		philos->number_times_to_eat = ft_atoi(av[5]);
+	else
+		philos->number_times_to_eat = -1;
+}
+
+void	set_philos(t_philosoph *philos, t_program *program,
+		pthread_mutex_t *forks, char **av)
+{
+	int	i;
+
+	i = 0;
+	while (i < ft_atoi(av[1]))
+	{
+		take_input(&philos[i], av);
+		philos[i].id = i + 1;
+		philos[i].eating = 0;
+		philos[i].meals_eaten = 0;
+		philos[i].start_time = get_current_time();
+		philos[i].last_meal = get_current_time();
+		philos[i].write_lock = &program->write_lock;
+		philos[i].dead_lock = &program->dead_lock;
+		philos[i].meal_lock = &program->meal_lock;
+		philos[i].dead = &program->dead_flag;
+		philos[i].l_fork = &forks[i];
+		if (i == 0)
+			philos[i].r_fork = &forks[philos[i].number_of_philos - 1];
+		else
+			philos[i].r_fork = &forks[i - 1];
+		i++;
+	}
+}
+
+void	init_forks(pthread_mutex_t *forks, int philos_number)
+{
+	int	i;
+
+	i = 0;
+	while (i < philos_number)
+	{
+		pthread_mutex_init(&forks[i], NULL);
+		i++;
+	}
+}
+
+void	init_program(t_program *program, t_philosoph *philos)
+{
+	program->dead_flag = 0;
+	program->philos = philos;
+	pthread_mutex_init(&program->write_lock, NULL);
+	pthread_mutex_init(&program->dead_lock, NULL);
+	pthread_mutex_init(&program->meal_lock, NULL);
+}
+
+void	destroy_all(char *s, t_program *program, pthread_mutex_t *forks)
+{
+	int	i;
+
+	i = 0;
+	if (s)
+	{
+		write(2, s, ft_strlen(s));
+		write(2, "\n", 1);
+	}
+	pthread_mutex_destroy(&program->write_lock);
+	pthread_mutex_destroy(&program->meal_lock);
+	pthread_mutex_destroy(&program->dead_lock);
+	while (i < program->philos[0].number_of_philos)
+	{
+		pthread_mutex_destroy(&forks[i]);
+		i++;
+	}
+}
---
+++ Philosoph/philos/main.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahamuyel <ahamuyel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/10 14:36:49 by ahamuyel          #+#    #+#             */
+/*   Updated: 2024/11/15 19:05:01 by ahamuyel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	if (s == NULL)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int	ft_atoi(char *s)
+{
+	unsigned long	n;
+	int				sig;
+	int				i;
+
+	n = 0;
+	sig = 1;
+	i = 0;
+	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == '\v'
+		|| s[i] == '\f' || s[i] == '\r')
+		i++;
+	if (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			sig = -1;
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		n = n * 10 + (s[i] - '0');
+		i++;
+	}
+	return (sig * n);
+}
+
+int	check_content_arg(char *av)
+{
+	int	i;
+
+	i = 0;
+	while (av[i])
+	{
+		if (av[i] < '0' || av[i] > '9')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	check_valid_args(char **av)
+{
+	if (ft_atoi(av[1]) > PHILO_MAX || ft_atoi(av[1]) <= 0
+		|| check_content_arg(av[1]))
+		return (write(2, "Invalid number of philosophers\n", 32), 1);
+	if (ft_atoi(av[2]) <= 0 || check_content_arg(av[2]))
+		return (write(2, "Invalid time to die\n", 21), 1);
+	if (ft_atoi(av[3]) <= 0 || check_content_arg(av[3]))
+		return (write(2, "Invalid time to eat\n", 21), 1);
+	if (ft_atoi(av[4]) <= 0 || check_content_arg(av[4]))
+		return (write(2, "Invalid time to sleep\n", 23), 1);
+	if (av[5] && (ft_atoi(av[5]) <= 0 || check_content_arg(av[5])))
+		return (write(2, "Invalid number of times each philosopher must eat\n",
+				51), 1);
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	t_program		program;
+	t_philosoph		philos[PHILO_MAX];
+	pthread_mutex_t	forks[PHILO_MAX];
+
+	if (ac != 5 && ac != 6)
+		return (write(2, "./philo [death] [meal] [spleep] [number of meals]\n",
+				51), 1);
+	if (check_valid_args(av))
+		return (1);
+	init_program(&program, philos);
+	init_forks(forks, ft_atoi(av[1]));
+	set_philos(philos, &program, forks, av);
+	create_thread(&program, forks);
+	destroy_all(NULL, &program, forks);
+	return (0);
+}
